ab94033ca60255c89cf2653a55fe84a4d67a55c1
divya2303 <114051737+divya2303@users.noreply.github.com>
2025-11-04 11:18:48 +0530

    Update Emails.js

--- backend/utils/Emails.js
+++ backend/utils/Emails.js
@@ -1,12 +1,15 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
   },
 });
+
 
 exports.sendMail = async(receiverEmail,subject,body) => {
     await transporter.sendMail({
