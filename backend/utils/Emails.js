3e2d5b00cdfa88e1ef03961055bf5a64e16b38a6
divya2303 <114051737+divya2303@users.noreply.github.com>
2025-11-04 11:52:30 +0530

    Update Emails.js

--- backend/utils/Emails.js
+++ backend/utils/Emails.js
@@ -1,13 +1,22 @@
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
+const testTransport = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
+    user: process.env.BREVO_EMAIL,
+    pass: process.env.BREVO_KEY,
   },
 });
+
+testTransport.verify((error, success) => {
+  if (error) {
+    console.error("❌ SMTP connection failed:", error);
+  } else {
+    console.log("✅ SMTP ready to send emails");
+  }
+});
+
 
 
 exports.sendMail = async(receiverEmail,subject,body) => {
