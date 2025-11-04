f6c977d8eef2e9f57bae5e17d93ef2655ec0a505
divya2303 <114051737+divya2303@users.noreply.github.com>
2025-11-04 11:33:38 +0530

    Update Emails.js

--- backend/utils/Emails.js
+++ backend/utils/Emails.js
@@ -1,9 +1,8 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp-relay.brevo.com",
+  port: 587,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
